8d227aff9d5d02b700e166a02b76e08604554902
moonkop <moonkop@qq.com>
2020-08-31 11:38:07 +0800

    webdav test

---
+++ config.js
@@ -0,0 +1,19 @@
+/*
+ * Example: for Nextcloud
+ * URL mynextcloud.tld:8008/nextcloud/remote.php/dav/files/julian
+ * Username: julian
+ * Password: password
+ * Port: 8008
+ * HTTPS: yes
+ */
+
+var config_username = '';
+var config_password = '';
+var config_port = '';
+
+// Note: config_base_url is crucial
+
+var config_url = '202.moonkop.com:' + config_port;
+var config_base_url = ''
+var config_https = true;
+
--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const { app, BrowserWindow } = require('electron')
 
-
+app.commandLine.appendSwitch('ignore-certificate-errors', 'true');
 function createWindow () {   
     // 创建浏览器窗口
     let win = new BrowserWindow({
       width: 800,
       height: 600,
       webPreferences: {
-        nodeIntegration: true
+        nodeIntegration: true,
+        plugins: true,
+        webSecurity: false,
+        allowDisplayingInsecureContent: true,
+        allowRunningInsecureContent: true
       }
     })
   
     // 加载index.html文件
-    win.loadURL('http://tools.moonkop.com/')
+    win.loadFile('index.html');
+    win.webContents.openDevTools();
+
   }
   
   app.whenReady().then(createWindow)
